df0e0edf1302faf26a84a456e66c7439142e931a
Codenames-Ren <renmizuki38@gmail.com>
2026-05-07 23:49:08 +0700

    add superadmin seeder

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class SuperAdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'id' => (string) Str::uuid(),
+            'name' => 'Gilbert',
+            'email' => 'gilbert@grpl.com',
+            'password' => Hash::make('Seadragon555'),
+            'role' => 'superadmin',
+            'is_active' => true,
+            'email_verified_at' => now(),
+        ]);
+    }
+}
