f1a2960f596ec607a7af020b3d49d437bac19927
Codenames-Ren <renmizuki38@gmail.com>
2026-05-07 23:47:19 +0700

    create model asesor

--- app/Models/Asesor.php
+++ app/Models/Asesor.php
@@ -3,8 +3,39 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Asesor extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'nama',
+        'nidn',
+        'bidang_keahlian',
+        'prodi_id',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class);
+    }
 }
