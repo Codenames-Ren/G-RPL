69542ce4fa933b60fe022507aa847f346ae88c71
Codenames-Ren <renmizuki38@gmail.com>
2026-05-07 23:47:48 +0700

    create model Rpl Manager

--- app/Models/RplManager.php
+++ app/Models/RplManager.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class RplManager extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'nama',
+        'jabatan',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
