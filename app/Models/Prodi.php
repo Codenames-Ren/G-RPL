56b2f321fda0bd12d335d8802012a24aa3ff6de8
Codenames-Ren <renmizuki38@gmail.com>
2026-05-12 00:17:55 +0700

    add relation to application module on model Prodi

--- app/Models/Prodi.php
+++ app/Models/Prodi.php
@@ -3,8 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Prodi extends Model
 {
-    //
+    protected $fillable = [
+        'nama_prodi',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function konsentrasis()
+    {
+        return $this->hasMany(Konsentrasi::class);
+    }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
