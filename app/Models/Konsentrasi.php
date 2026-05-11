6a498dce1a9fc37327c3d0ff0e2833a2cf4b8c28
Codenames-Ren <renmizuki38@gmail.com>
2026-05-12 00:18:20 +0700

    add relation to application module on model Konsentrasi

--- app/Models/Konsentrasi.php
+++ app/Models/Konsentrasi.php
@@ -3,8 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Konsentrasi extends Model
 {
-    //
+    protected $fillable = [
+        'prodi_id',
+        'nama_konsentrasi',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class);
+    }
+
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
 }
