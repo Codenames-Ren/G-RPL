04fcf71907b1e8793ea3a512b4bd4ed3e020a40b
Codenames-Ren <renmizuki38@gmail.com>
2026-05-12 00:20:17 +0700

    initialize model learning experience

--- app/Models/LearningExperience.php
+++ app/Models/LearningExperience.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class LearningExperience extends Model
 {
-    //
+    protected $fillable = [
+        'application_id',
+        'title',
+        'type',
+        'description',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function application()
+    {
+        return $this->belongsTo(Application::class);
+    }
 }
