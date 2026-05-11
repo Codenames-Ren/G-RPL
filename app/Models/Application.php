945641cb6b4c13a2611eec04ebf3ecce4a9b3e1f
Codenames-Ren <renmizuki38@gmail.com>
2026-05-12 00:19:19 +0700

    initialize model application

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -3,8 +3,54 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Application extends Model
 {
-    //
+    protected $fillable = [
+        'applicant_id',
+        'jenis_rpl',
+        'prodi_id',
+        'konsentrasi_id',
+        'status',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function applicant()
+    {
+        return $this->belongsTo(Applicant::class);
+    }
+
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class);
+    }
+
+    public function konsentrasi()
+    {
+        return $this->belongsTo(Konsentrasi::class);
+    }
+
+    public function documents()
+    {
+        return $this->hasMany(ApplicationDocument::class);
+    }
+
+    public function learningExperiences()
+    {
+        return $this->hasMany(LearningExperience::class);
+    }
 }
