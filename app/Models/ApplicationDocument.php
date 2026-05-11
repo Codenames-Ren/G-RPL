c701f3c2301798f579dbc579fc84952009664297
Codenames-Ren <renmizuki38@gmail.com>
2026-05-12 00:19:44 +0700

    initialize model application document

--- app/Models/ApplicationDocument.php
+++ app/Models/ApplicationDocument.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class ApplicationDocument extends Model
 {
-    //
+    protected $fillable = [
+        'application_id',
+        'type',
+        'file_path',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function application()
+    {
+        return $this->belongsTo(Application::class);
+    }
 }
