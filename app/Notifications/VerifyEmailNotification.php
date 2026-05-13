46953c3eae216c6938b71cfbbfd8e74aeed47657
Codenames-Ren <renmizuki38@gmail.com>
2026-05-14 00:56:28 +0700

    create custom notifications

---
+++ app/Notifications/VerifyEmailNotification.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\URL;
+
+class VerifyEmailNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        // generate signed verification URL
+        $verifyUrl = URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(60),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1(
+                    $notifiable->getEmailForVerification()
+                ),
+            ]
+        );
+
+        return (new MailMessage)
+            ->subject('Verify Your G-RPL Account')
+            ->greeting('Hello, ' . $notifiable->name . '!')
+            ->line('Thank you for registering to G-RPL.')
+            ->line('Please verify your email address to continue using the system.')
+            ->action('Verify Email', $verifyUrl)
+            ->line('This verification link will expire in 60 minutes.')
+            ->line('If you did not create an account, no further action is required.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
