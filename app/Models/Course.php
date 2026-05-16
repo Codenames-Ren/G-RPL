67d045fea500b5ef0b6e998f80591ca3f878c79d
Codenames-Ren <renmizuki38@gmail.com>
2026-05-16 16:57:39 +0700

    create models course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,44 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Course extends Model
 {
-    //
+    protected $fillable = [
+        'prodi_id',
+        'konsentrasi_id',
+        'kode_mk',
+        'nama_matkul',
+        'sks',
+    ];
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class);
+    }
+
+    public function konsentrasi()
+    {
+        return $this->belongsTo(Konsentrasi::class);
+    }
+
+    public function assessmentDetails()
+    {
+        return $this->hasMany(AssessmentDetail::class);
+    }
 }
