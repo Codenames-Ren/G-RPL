a06f2c82b8ba7261f770b59aa0dde2098292ba90
Codenames-Ren <renmizuki38@gmail.com>
2026-05-18 23:31:27 +0700

    adjust header email notification

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,45 @@
 @props(['url'])
+
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo-v2.1.png" class="logo" alt="Laravel Logo">
-@else
-{!! $slot !!}
-@endif
+
+<a href="{{ $url }}" style="display: inline-block; text-align: center;">
+
+    {{-- Logo G-RPL --}}
+    <img
+        src="{{ asset('images/logo.png') }}"
+        class="logo"
+        alt="G-RPL Logo"
+        style="
+            height: 80px;
+            width: 80px;
+            object-fit: contain;
+            margin-bottom: 10px;
+        "
+    >
+
+    {{-- System Name --}}
+    <div style="
+        color: #1565C0;
+        font-size: 24px;
+        font-weight: 700;
+        margin-top: 8px;
+        letter-spacing: 0.5px;
+    ">
+        G-RPL
+    </div>
+
+    {{-- Subtitle --}}
+    <div style="
+        color: #64748b;
+        font-size: 13px;
+        margin-top: 4px;
+        font-weight: 500;
+    ">
+        Sistem Rekognisi Pembelajaran Lampau
+    </div>
+
 </a>
+
 </td>
 </tr>
