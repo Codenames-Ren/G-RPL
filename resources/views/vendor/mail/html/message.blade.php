05a116c9080165facd37e985ffdedc6ad1554f28
Codenames-Ren <renmizuki38@gmail.com>
2026-05-18 23:32:02 +0700

    adjust message email notification

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,8 +1,9 @@
 <x-mail::layout>
+
 {{-- Header --}}
 <x-slot:header>
 <x-mail::header :url="config('app.url')">
-{{ config('app.name') }}
+G-RPL
 </x-mail::header>
 </x-slot:header>
 
@@ -12,16 +13,46 @@
 {{-- Subcopy --}}
 @isset($subcopy)
 <x-slot:subcopy>
+
 <x-mail::subcopy>
+
+<div style="
+    color: #64748b;
+    font-size: 13px;
+    line-height: 1.6;
+">
+
 {!! $subcopy !!}
+
+</div>
+
 </x-mail::subcopy>
+
 </x-slot:subcopy>
 @endisset
 
 {{-- Footer --}}
 <x-slot:footer>
+
 <x-mail::footer>
-© {{ date('Y') }} {{ config('app.name') }}. {{ __('All rights reserved.') }}
+
+<div style="
+    text-align: center;
+    color: #94a3b8;
+    font-size: 12px;
+    line-height: 1.7;
+">
+
+© {{ date('Y') }} G-RPL System <br>
+
+Sistem Rekognisi Pembelajaran Lampau <br>
+
+Global Institute
+
+</div>
+
 </x-mail::footer>
+
 </x-slot:footer>
+
 </x-mail::layout>
