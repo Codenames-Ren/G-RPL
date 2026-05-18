967817c55606c18206bfb95f185c67db10e86f86
Codenames-Ren <renmizuki38@gmail.com>
2026-05-18 23:32:26 +0700

    adjust footer in email notification

--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -1,11 +1,58 @@
 <tr>
 <td>
-<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+
+<table
+    class="footer"
+    align="center"
+    width="570"
+    cellpadding="0"
+    cellspacing="0"
+    role="presentation"
+>
+
 <tr>
-<td class="content-cell" align="center">
-{{ Illuminate\Mail\Markdown::parse($slot) }}
+
+<td
+    class="content-cell"
+    align="center"
+    style="
+        padding-top: 24px;
+        padding-bottom: 24px;
+    "
+>
+
+<div style="
+    text-align: center;
+    color: #94a3b8;
+    font-size: 12px;
+    line-height: 1.8;
+">
+
+<strong style="
+    color: #1565C0;
+    font-size: 13px;
+">
+    G-RPL System
+</strong>
+
+<br>
+
+Sistem Rekognisi Pembelajaran Lampau
+
+<br>
+
+Global Institute
+<br><br>
+
+© {{ date('Y') }} G-RPL. Seluruh hak cipta dilindungi.
+
+</div>
+
 </td>
+
 </tr>
+
 </table>
+
 </td>
 </tr>
